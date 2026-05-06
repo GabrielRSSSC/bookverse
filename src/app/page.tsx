88f96f81be4d65e04e3206eab8f21026e894e51e
GabrielRSSSC <gabrielrodriguescitizen@gmail.com>
2026-05-06 11:31:41 -0300

    Upando

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Main(){
+    return(
+        <main>
+            <h1>Oi</h1>
+            <p>Eae</p>
+        </main>
+    )
+}
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import { features } from 'process';
+import {Container} from './styles';
+export default function Header(){
+    return(
+        <Container>
+            <h1>Meu projetinho</h1>
+        </Container>
+    )
+}
